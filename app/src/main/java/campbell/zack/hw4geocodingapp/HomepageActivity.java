b9270ec820650ce1968ddf1ddb61008d48d5cf97
Zack <zack.c.campbell@gmail.com>
2018-10-27 20:01:08 -0500

    Created activity and connected button to maps activity

--- app/src/main/java/campbell/zack/hw4geocodingapp/HomepageActivity.java
+++ app/src/main/java/campbell/zack/hw4geocodingapp/HomepageActivity.java
@@ -1,4 +1,23 @@
 package campbell.zack.hw4geocodingapp;
 
-public class HomepageActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomepageActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void findLoc(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+
+        // USE putExtra() on the intent to send processed data to the map
+
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/campbell/zack/hw4geocodingapp/MapsActivity.java
+++ app/src/main/java/campbell/zack/hw4geocodingapp/MapsActivity.java
@@ -1,5 +1,6 @@
 package campbell.zack.hw4geocodingapp;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -18,6 +19,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Intent intent = getIntent();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
